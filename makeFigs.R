6bbdd34f7a68ca56b8ad8332bdfc96adfdc8adc4
Colin Olito <colin.olito@gmail.com>
2022-09-23 11:53:24 +0200

    Cleanup executables

--- makeFigs.R
+++ makeFigs.R
@@ -34,23 +34,18 @@ source('R/functions-figures.R')
 #' Main Figures
 ########################
 
-# Replaced with Combined fig
-# toPdf(pFixFig_Neutral_Ben(), figPath(name='PrFix_neutral_ben_delMut.pdf'), width=5, height=10)
-# embed_fonts(figPath(name='PrFix_neutral_ben_delMut.pdf'))
+# Fig 1. Illustrative diagram figure
+# Created in ppt
 
-
-# Preferred fig including Pr(catch SA) below
-# toPdf(pFixFig_combined(), figPath(name='PrFix_combined_delMut.pdf'), width=8, height=6)
-# embed_fonts(figPath(name='PrFix_combined_delMut.pdf'))
-
-
+# Fig 2. Pr(fix | x) ~ inversion Size
  toPdf(pFix_pCatchSAFig_combined(), figPath(name='PrFix_PrCatchSA_combined_delMut.pdf'), width=7, height=7)
  embed_fonts(figPath(name='PrFix_PrCatchSA_combined_delMut.pdf'))
 
+# Fig 3. Pr(fix | x, catch SLR) ~ inversion Size
  toPdf(PrSpanSLRFixFigure(), figPath(name='PrFix_SpanSLR_combined_delMut.pdf'), width=10, height=7)
  embed_fonts(figPath(name='PrFix_SpanSLR_combined_delMut.pdf'))
 
-source('R/functions-figures.R')
+# Fig 4. Expected distribution of fixed inveresion lengths: low mutation load
  toPdf(expectedInvDistFig(Ud=0.02), figPath(name='Expected_xDistribution_delMut_Ud0_02.pdf'), width=7, height=7)
  embed_fonts(figPath(name='Expected_xDistribution_delMut_Ud0_02.pdf'))
 
@@ -61,49 +56,56 @@ source('R/functions-figures.R')
 #' Supplementary Figures
 ########################
 
+# Effect of r_SA on fixation probabilities
  toPdf(pFixFig_SA_r(), figPath(name='PrFix_SA_r_delMut.pdf'), width=8, height=6)
  embed_fonts(figPath(name='PrFix_SA_r_delMut.pdf'))
 
+# Effect of r_SA on fixation probabilities, w/ biased selection
  toPdf(pFixFig_SA_biasedSel_r(), figPath(name='PrFix_biasedSA_r_delMut.pdf'), width=8, height=4)
  embed_fonts(figPath(name='PrFix_biasedSA_r_delMut.pdf'))
 
-
-source('R/functions-figures.R')
-
+# Illustrative figure of Pr(catch SLR) ~ x
  toPdf(PrSpanSLR_Fig(), figPath(name='PrSpanSLR_SuppFig.pdf'), width=7, height=4)
  embed_fonts(figPath(name='PrSpanSLR_SuppFig.pdf'))
 
-
-
-source('R/functions-figures.R')
+# Expected distribution of fixed inveresion lengths: intermediate mutation load
  toPdf(expectedInvDistFig(Ud=0.05), figPath(name='Expected_xDistribution_delMut_Ud0_05.pdf'), width=7, height=7)
  embed_fonts(figPath(name='Expected_xDistribution_delMut_Ud0_05.pdf'))
 
+# Expected distribution of fixed inveresion lengths: high mutation load
  toPdf(expectedInvDistFig(Ud=0.1), figPath(name='Expected_xDistribution_delMut_Ud0_1.pdf'), width=7, height=7)
  embed_fonts(figPath(name='Expected_xDistribution_delMut_Ud0_1.pdf'))
+
+
 
 ########################
 # Old Figures
 ########################
 
+# Replaced with Combined fig
+# toPdf(pFixFig_Neutral_Ben(), figPath(name='PrFix_neutral_ben_delMut.pdf'), width=5, height=10)
+# embed_fonts(figPath(name='PrFix_neutral_ben_delMut.pdf'))
 
- toPdf(fixationProbabilityFigure(), figPath(name='Fig1.pdf'), width=10, height=4)
- embed_fonts(figPath(name='Fig1.pdf'))
+
+# Preferred fig including Pr(catch SA) below
+# toPdf(pFixFig_combined(), figPath(name='PrFix_combined_delMut.pdf'), width=8, height=6)
+# embed_fonts(figPath(name='PrFix_combined_delMut.pdf'))
+
+# toPdf(fixationProbabilityFigure(), figPath(name='Fig1.pdf'), width=10, height=4)
+# embed_fonts(figPath(name='Fig1.pdf'))
 
 # Pr(fix | x, SDR)
- toPdf(PrCatchFixFigure(SDRloc = c(1/2, 1/10)), figPath(name='PrCatchFixFig.pdf'), width=10, height=8)
- embed_fonts(figPath(name='PrCatchFixFig.pdf'))
+# toPdf(PrCatchFixFigure(SDRloc = c(1/2, 1/10)), figPath(name='PrCatchFixFig.pdf'), width=10, height=8)
+# embed_fonts(figPath(name='PrCatchFixFig.pdf'))
 
- toPdf(expectedDistributionFig(SDRloc = c(1/2, 1/10)), figPath(name='expDistFig.pdf'), width=7, height=7)
- embed_fonts(figPath(name='expDistFig.pdf'))
+# toPdf(expectedDistributionFig(SDRloc = c(1/2, 1/10)), figPath(name='expDistFig.pdf'), width=7, height=7)
+# embed_fonts(figPath(name='expDistFig.pdf'))
 
-source('R/functions-figures.R')
- toPdf(determInvFreqPlot(wesMovie = 'Zissou1'), figPath(name='detEqInvFreqFig.pdf'), width=10, height=10)
- embed_fonts(figPath(name='detEqInvFreqFig.pdf'))
+# toPdf(determInvFreqPlot(wesMovie = 'Zissou1'), figPath(name='detEqInvFreqFig.pdf'), width=10, height=10)
+# embed_fonts(figPath(name='detEqInvFreqFig.pdf'))
 
-source('R/functions-figures.R')
- toPdf(recombEffectSATwoLocus(), figPath(name='recombEffect.pdf'), width=10, height=6)
- embed_fonts(figPath(name='recombEffect.pdf'))
+# toPdf(recombEffectSATwoLocus(), figPath(name='recombEffect.pdf'), width=10, height=6)
+# embed_fonts(figPath(name='recombEffect.pdf'))
 
 
 # toPdf(Fig3Alt(), 
@@ -122,11 +124,6 @@ source('R/functions-figures.R')
 #             figPath(name='FinalInvFreqSuppFig.pdf'), width=7, height=7)
 # embed_fonts(figPath(name='FinalInvFreqSuppFig.pdf'))
 
-########################
-# Supplementary Figures
-########################
-
-
 
 
 
@@ -137,7 +134,7 @@ source('R/functions-figures.R')
 
 # Validation of analytic approximations for the SA model 
 # invasion probability of Y-linked inversions
-toPdf(SA_Y_validatePinv(s.df = "SA_Y_validatePinv_sfGrad_N30000_hf0.5_hm0.5_sm0.01_r0.5.csv",
-				  r.df = "SA_Y_validatePinv_rGrad_N30000_hf0.5_sf0.01_hm0.5_sm0.01.csv"), 
-      figPath(name='SA_Y_validatePinv.pdf'), width=10, height=5)
-embed_fonts(figPath(name='SA_Y_validatePinv.pdf'))
+# toPdf(SA_Y_validatePinv(s.df = "SA_Y_validatePinv_sfGrad_N30000_hf0.5_hm0.5_sm0.01_r0.5.csv",
+# 				  r.df = "SA_Y_validatePinv_rGrad_N30000_hf0.5_sf0.01_hm0.5_sm0.01.csv"), 
+#       figPath(name='SA_Y_validatePinv.pdf'), width=10, height=5)
+# embed_fonts(figPath(name='SA_Y_validatePinv.pdf'))
--- run-WFSims-prFix-recDel.R
+++ run-WFSims-prFix-recDel.R
@@ -10,22 +10,20 @@
 #  NOTES:  
 #		
 
-
-
 ######################################
 #' Create output directories if they
 #' do not already exist
-#dataDirectoryExists  <-  dir.exists(".output/data")
+dataDirectoryExists  <-  dir.exists(".output/data")
 
-#if(!dataDirectoryExists) {
-#	dir.create("./data")
-#}
+if(!dataDirectoryExists) {
+	dir.create("./data/simResults")
+}
 
-#figuresDirectoryExists  <-  dir.exists("./output/figures")
+figuresDirectoryExists  <-  dir.exists("./output/figures")
 
-#if(!figuresDirectoryExists) {
-#	dir.create("./output/figures")
-#}
+if(!figuresDirectoryExists) {
+	dir.create("./output/figures")
+}
 
 ########################################
 # Fixation Probability ~ Inv. Size
@@ -209,12 +207,16 @@ for(i in 1:nrow(eqFreq.subdat)) {
 
 
 
-# >> CONTINUE RUNNING SIMULATIONS FROM HERE
 
 
 ################################################################
 ################################################################
 ##  End of new sim code for partially recessive del. mut.
+##  
+##  WARNING: All code after this is functional but deprecated!
+##  		 It will produce results from a much earlier 
+##			 version of the manuscript which focused on 
+##			 additive deleterious mutations.
 ################################################################
 ################################################################
 
@@ -237,8 +239,8 @@ for(i in 1:nrow(eqFreq.subdat)) {
 
 #####################
 ##  Dependencies
-rm(list=ls())
-source('R/functions-Neutral-WFSims.R')
+# rm(list=ls())
+# source('R/functions-Neutral-WFSims.R')
 
 
 
@@ -250,122 +252,122 @@ source('R/functions-Neutral-WFSims.R')
 ############
 # Neutral
 
-N_y = 100
-par.list  <-  list(Ny   = 10^2,
-				   sd   = 0.05,
-				   Ud   = 0.2)
-neutralFwdSimY(par.list=par.list)
+#N_y = 100
+#par.list  <-  list(Ny   = 10^2,
+#				   sd   = 0.05,
+#				   Ud   = 0.2)
+#neutralFwdSimY(par.list=par.list)
 
 # N_y = 500
-par.list  <-  list(Ny   = 5*10^2,
-				   sd   = 0.05,
-				   Ud   = 0.2)
-neutralFwdSimY(par.list=par.list)
+#par.list  <-  list(Ny   = 5*10^2,
+#				   sd   = 0.05,
+#				   Ud   = 0.2)
+#neutralFwdSimY(par.list=par.list)
 
 # N_y = 750
-par.list  <-  list(Ny   = 7.5*10^2,
-				   sd   = 0.05,
-				   Ud   = 0.2)
-neutralFwdSimY(par.list=par.list)
+#par.list  <-  list(Ny   = 7.5*10^2,
+#				   sd   = 0.05,
+#				   Ud   = 0.2)
+#neutralFwdSimY(par.list=par.list)
 
 # N_y = 1000
-par.list  <-  list(Ny   = 10^3,
-				   sd   = 0.05,
-				   Ud   = 0.2)
-neutralFwdSimY(par.list=par.list)
+#par.list  <-  list(Ny   = 10^3,
+#				   sd   = 0.05,
+#				   Ud   = 0.2)
+#neutralFwdSimY(par.list=par.list)
 
 # N_y = 10000
-par.list  <-  list(Ny   = 10^4,
-				   sd   = 0.05,
-				   Ud   = 0.2)
-neutralFwdSimY(par.list=par.list)
+#par.list  <-  list(Ny   = 10^4,
+#				   sd   = 0.05,
+#				   Ud   = 0.2)
+#neutralFwdSimY(par.list=par.list)
 
 
 
 ############
 # Beneficial
-rm(list=ls())
-source('R/functions-Beneficial-WFSims.R')
+#rm(list=ls())
+#source('R/functions-Beneficial-WFSims.R')
 
 # N_y = 1000
-par.list  <-  list(Ny   = 10^3,
-				   sI   = 0.02,
-				   sd   = 0.02,
-				   Ud   = 0.2)
-beneficialFwdSimY(par.list=par.list)
+#par.list  <-  list(Ny   = 10^3,
+#				   sI   = 0.02,
+#				   sd   = 0.02,
+#				   Ud   = 0.2)
+#beneficialFwdSimY(par.list=par.list)
 
 # N_y = 1000
-par.list  <-  list(Ny   = 10^3,
-				   sI   = 0.02,
-				   sd   = 0.03,
-				   Ud   = 0.2)
-beneficialFwdSimY(par.list=par.list)
+#par.list  <-  list(Ny   = 10^3,
+#				   sI   = 0.02,
+#				   sd   = 0.03,
+#				   Ud   = 0.2)
+#beneficialFwdSimY(par.list=par.list)
 
 # N_y = 10000
-par.list  <-  list(Ny   = 10^4,
-				   sI   = 0.02,
-				   sd   = 0.02,
-				   Ud   = 0.2)
-beneficialFwdSimY(par.list=par.list)
+#par.list  <-  list(Ny   = 10^4,
+#				   sI   = 0.02,
+#				   sd   = 0.02,
+#				   Ud   = 0.2)
+#beneficialFwdSimY(par.list=par.list)
 
 
 
 ############
 # SA Selection
-rm(list=ls())
-source('R/functions-SA-Y-2Locus-determSims.R')
-source('R/functions-SA-Y-2Locus-WFSims.R')
+#rm(list=ls())
+#source('R/functions-SA-Y-2Locus-determSims.R')
+#source('R/functions-SA-Y-2Locus-WFSims.R')
 
-Ud          <-  0.1
-sd          <-  0.01
-hf          <-  1/2
-sf          <-  0.05
-hm          <-  1/2
-sm          <-  0.05
-r_linked    <-  0.0101
-r_unlinked  <-  1/2
-A           <-  1
-P           <-  0.05
+# Ud          <-  0.1
+# sd          <-  0.01
+# hf          <-  1/2
+# sf          <-  0.05
+# hm          <-  1/2
+# sm          <-  0.05
+# r_linked    <-  0.0101
+# r_unlinked  <-  1/2
+# A           <-  1
+# P           <-  0.05
 
 ## SA locus initially unlinked with SDL 
 # N_y = 10^3
- par.list  <-  list(
-					gen     =  10^4,
-					N       =  10^4,
-					sm      =  sm,
-					sf      =  sf,
-					hm      =  hm,
-					hf      =  hf,
-					r       =  r_unlinked,
-					Ud      =  Ud,
-					sd      =  sd,
-					x       =  NA,
-					selType =  "SA",
-					selPars =  c(hf, sf, hm, sm),
-					A       =  A,
-					P       =  P
-					)
-SAFwdSimYpFixFig(par.list=par.list)
+# par.list  <-  list(
+#					gen     =  10^4,
+#					N       =  10^4,
+#					sm      =  sm,
+#					sf      =  sf,
+#					hm      =  hm,
+#					hf      =  hf,
+#					r       =  r_unlinked,
+#					Ud      =  Ud,
+#					sd      =  sd,
+#					x       =  NA,
+#					selType =  "SA",
+#					selPars =  c(hf, sf, hm, sm),
+#					A       =  A,
+#					P       =  P
+#					)
+#SAFwdSimYpFixFig(par.list=par.list)
 
 
 
 
 ## SA locus initially LINKED with SDL 
 # N_y = 10^3
- par.list  <-  list(
-					gen     =  10^4,
-					N       =  10^4,
-					sm      =  sm,
-					sf      =  sf,
-					hm      =  hm,
-					hf      =  hf,
-					r       =  r_linked,
-					Ud      =  Ud,
-					sd      =  sd,
-					x       =  NA,
-					selType =  "SA",
-					selPars =  c(hf, sf, hm, sm),
-					A       =  A,
-					P       =  P
-					)
-SAFwdSimYpFixFig(par.list=par.list)
+#  par.list  <-  list(
+# 					gen     =  10^4,
+# 					N       =  10^4,
+# 					sm      =  sm,
+# 					sf      =  sf,
+# 					hm      =  hm,
+# 					hf      =  hf,
+# 					r       =  r_linked,
+# 					Ud      =  Ud,
+# 					sd      =  sd,
+# 					x       =  NA,
+# 					selType =  "SA",
+# 					selPars =  c(hf, sf, hm, sm),
+# 					A       =  A,
+# 					P       =  P
+# 					)
+# SAFwdSimYpFixFig(par.list=par.list)
